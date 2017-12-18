cd4597421589cafc016d115d58b879d4bb555b7d
kaminska4nna <kaminska4nna@users.noreply.github.com>
2017-12-18 12:38:11 +0100

    commit

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -16,7 +17,12 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTaskById(Long id){
-        return repository.findAllById(id);
+
+    public Task saveTask(final Task task){
+        return repository.save(task);
+    }
+
+    public Optional<Task> getTask(Long id){
+        return repository.findById(id);
     }
 }
